05dfd0e9958fa0004078442407fa8a25ea0179c2
wordpress219 <913207726@qq.com>
2018-11-06 11:54:22 +0800

    Add files via upload
    第一次C语言实验课的代码，写的不是很好，但是都已经调试过可以用，容错做的不太好。。。

---
+++ codes/exp3_4.c
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int isPrime(int a)
+{
+	if ((a > 100) && (a % 2 == 0))
+	{
+		return 0;
+	}
+	else if ((a > 100) && (a % 2 != 0))
+	{
+		return 1;
+	}
+	for (int k = 2; k <= (int)sqrt(a); k++)
+	{
+		if (a%k == 0)
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+int main()
+{
+	printf("请输入一个正整数\n");
+	int a;
+	scanf("%d", &a);
+	if (a <= 0)
+	{
+		printf("输入错误！\n");
+		system("pause");
+		exit(0);
+	}
+	if (a = 1)
+	{
+		printf("您输入的数既不是质数也不是合数！\n");
+		system("pause");
+		exit(0);
+	}
+	int r = isPrime(a);
+	if (r == 0)
+	{
+		printf("您输入的数不是质数！\n");
+	}
+	else if (r == 1)
+	{
+		printf("您输入的数是质数！\n");
+	}
+	system("pause");
+	return 0;
+}
